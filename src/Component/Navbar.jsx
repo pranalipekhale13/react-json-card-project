1ec1e3d49a048dc0d3f83f6172209b6ca67ad987
pranalipekhale13 <pekhalepranali133@gmail.com>
2026-06-25 23:32:25 +0530

    added logo

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,7 +1,7 @@
 import "./Navbar.css";
 import { Link, useLocation } from "react-router-dom";
 import { House, PhoneCall, ListCollapse } from "lucide-react";
-
+import logo from "../assets/sporthub-logo.png";
 function Navbar() {
   const location = useLocation();
 
@@ -9,6 +9,11 @@ function Navbar() {
 
   return (
     <div className="navbar">
+      <Link to="/" className="logo-container">
+        <img src={logo} alt="SportsHub Logo" className="logo-image" />
+        
+      </Link>
+
       <Link
         to="/"
         className={`nav-menu${isActive("/") ? " active-menu" : ""}`}
